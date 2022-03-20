824333a1b9616be11b216282bcfa0e949c6fc529
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-03-20 17:53:19 +0100

    updates generic view utility

--- user/utils.py
+++ user/utils.py
@@ -9,8 +9,8 @@ class BaseModel(models.Model):
     id = models.UUIDField(
         primary_key=True, db_index=True, default=uuid.uuid4(), editable=False
     )
-    created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
