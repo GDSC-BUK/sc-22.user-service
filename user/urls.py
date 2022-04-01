5f66f256e52dbb7d9b92ca7c496cd8dce6b2988e
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-04-02 00:43:22 +0100

    updates documentation path

--- user/urls.py
+++ user/urls.py
@@ -23,11 +23,6 @@ urlpatterns = [
     path("api/v1/", include("core.urls")),
     path(
         "api/v1/docs/",
-        schema_view.with_ui("redoc", cache_timeout=0),
-        name="schema-redoc",
-    ),
-    path(
-        "api/v1/docs/swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
