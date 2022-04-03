e5189c00378f45dde7e487177c125c74e9bb2cf4
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-04-03 10:09:04 +0100

    updates base model id field

--- user/utils.py
+++ user/utils.py
@@ -7,7 +7,7 @@ import uuid, re
 class BaseModel(models.Model):
 
     id = models.UUIDField(
-        primary_key=True, db_index=True, default=uuid.uuid4(), editable=False
+        primary_key=True, db_index=True, default=uuid.uuid4, editable=False
     )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
